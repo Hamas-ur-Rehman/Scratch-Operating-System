cb33902b1708122036fec580a4db6773d232f390
Hamas-ur-Rehman <hamasurrehman@gmail.com>
2022-11-19 19:51:12 +0500

    bug fix

--- src/main.asm
+++ src/main.asm
@@ -1,50 +1,67 @@
 org 0x7C00
 bits 16
 
+
 %define ENDL 0x0D, 0x0A
 
-start: 
-    jmp
 
-;prints characters to the screen
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
+    ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb
-    or al,al
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah,0x0E
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
+
     jmp .loop
+
 .done:
+    pop bx
     pop ax
-    pop si
+    pop si    
     ret
+    
 
 main:
-    ;setup data segment
-    mov ax,0
-    mov ds,ax
-    mov es,ax
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
-    ;setup stack segment
-    mov ss,ax
-    mov sp,0x7C00
-
-    ;print message
+    ; print hello world message
     mov si, msg_hello
     call puts
 
     hlt
 
-.halt:
+.halt
     jmp .halt
 
 
-msg_hello: db 'Hello world from hamas in 16 bit mode!',ENDL,0
+
+msg_hello: db 'Hello world from hamas in 16 bit mode!', ENDL, 0
+
 
 times 510-($-$$) db 0
 dw 0AA55h
+

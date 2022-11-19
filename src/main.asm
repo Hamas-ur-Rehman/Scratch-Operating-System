fe62c3f88f87c9dc035bb9cbdee3ee12b8609f80
Hamas-ur-Rehman <hamasurrehman@gmail.com>
2022-11-19 19:30:34 +0500

    added hello world to the screen

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,50 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start: 
+    jmp
+
+;prints characters to the screen
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al,al
+    jz .done
+
+    mov ah,0x0E
+    int 0x10
+    jmp .loop
+.done:
+    pop ax
+    pop si
+    ret
 
 main:
+    ;setup data segment
+    mov ax,0
+    mov ds,ax
+    mov es,ax
+
+    ;setup stack segment
+    mov ss,ax
+    mov sp,0x7C00
+
+    ;print message
+    mov si, msg_hello
+    call puts
+
     hlt
 
 .halt:
     jmp .halt
 
+
+msg_hello: db 'Hello world from hamas in 16 bit mode!',ENDL,0
 
 times 510-($-$$) db 0
 dw 0AA55h
